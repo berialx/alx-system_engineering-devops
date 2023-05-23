978f8e99b7a8ce40f2974c1629cbefeb604a6860
berialx <emailbereket@gmail.com>
2023-05-23 11:59:42 +0000

    tasks

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable User Holberton to accoss and open files without errors
+
+# Increase hard-file limit for Holberton User
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Inc soft file limit for Holberton User
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
